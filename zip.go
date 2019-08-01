b3e422390e27d8b2f1f8f6018744eac50c6b79cc
Suzhen <suzhen@homeinpocket.net>
2019-08-01 10:54:05 +0800

    :sparkles: add Zip

--- zip.go
+++ zip.go
@@ -2,10 +2,113 @@ package std
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 )
+
+type FileGroup struct {
+	rawDir          string
+	dirName         string
+	childFiles      []string
+	childFileGroups []*FileGroup
+}
+
+func (this *FileGroup) GetDirName() string {
+	return this.dirName
+}
+
+func (this *FileGroup) GetChildFiles() []string {
+	return this.childFiles
+}
+
+func (this *FileGroup) SetCustomDirName(dirName string) *FileGroup {
+	this.dirName = dirName + separatorStr
+	return this
+}
+
+const separatorStr = string(filepath.Separator)
+
+func NewFileGroup(dir string, childFiles ...string) *FileGroup {
+	out := &FileGroup{
+		rawDir:     dir,
+		dirName:    "",
+		childFiles: nil,
+	}
+	if len(dir) != 0 {
+		out.dirName = fmt.Sprintf("%s%c", filepath.Base(dir), filepath.Separator)
+	}
+	if len(childFiles) == 0 {
+		return out
+	}
+	out.childFiles = make([]string, 0, len(childFiles))
+	for idx := range childFiles {
+		out.childFiles = append(out.childFiles, filepath.Base(childFiles[idx]))
+	}
+	return out
+}
+
+func (this *FileGroup) addChildFileGroup(fg FileGroup) {
+	fg.rawDir = filepath.Join(this.rawDir, filepath.Base(fg.rawDir))
+	fg.dirName = filepath.Join(this.dirName, fg.dirName) + separatorStr
+	this.childFileGroups = append(this.childFileGroups, &fg)
+}
+
+func (this *FileGroup) AddChildFileGroup(fgs ...*FileGroup) *FileGroup {
+	if this.childFileGroups == nil {
+		this.childFileGroups = make([]*FileGroup, 0, len(fgs))
+	}
+	for _, fg := range fgs {
+		this.addChildFileGroup(*fg)
+	}
+	return this
+}
+
+func zipFilesDetails(zipWriter *zip.Writer, fgs []*FileGroup) error {
+	curDir := "."
+	for _, fg := range fgs {
+		if len(fg.dirName) != 0 {
+			_, err := zipWriter.Create(fg.dirName)
+			if err != nil {
+				return err
+			}
+			curDir = fg.dirName
+		} else {
+			curDir = "."
+		}
+		for _, fName := range fg.childFiles {
+			writer, err := zipWriter.Create(filepath.Join(curDir, fName))
+			if err != nil {
+				return err
+			}
+			file, err := os.Open(filepath.Join(fg.rawDir, fName))
+			if err != nil {
+				return err
+			}
+			_, err = io.Copy(writer, file)
+			if err != nil {
+				_ = file.Close()
+				return err
+			}
+			_ = file.Close()
+		}
+		if len(fg.childFileGroups) == 0 {
+			continue
+		}
+		err := zipFilesDetails(zipWriter, fg.childFileGroups)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func Zip(output io.Writer, fgs []*FileGroup) error {
+	zipWriter := zip.NewWriter(output)
+	defer CloseIgnoreErr(zipWriter) // implicit flush to file
+	return zipFilesDetails(zipWriter, fgs)
+}
 
 func UnZip(zipFilePath, output string) error {
 	zrc, err := zip.OpenReader(zipFilePath)
--- zip_test.go
+++ zip_test.go
@@ -1,6 +1,8 @@
 package std
 
 import (
+	"archive/zip"
+	"fmt"
 	"os"
 	"testing"
 )
@@ -13,4 +15,39 @@ func TestUnZip(t *testing.T) {
 	}()
 	err = UnZip("sample.zip", "zip_output")
 	AssertError(err, "unzip")
+}
+
+func TestZip(t *testing.T) {
+	wd, err := os.Getwd()
+	AssertError(err, "getwd")
+	fmt.Println("wd=", wd)
+	file, err := os.OpenFile("output.zip", os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0644)
+	AssertError(err, "create output")
+	defer func() {
+		_ = os.Remove("output.zip")
+	}()
+	err = Zip(file, []*FileGroup{
+		NewFileGroup(".", "go.mod", "go.sum").SetCustomDirName("c"),
+		NewFileGroup(".idea", "misc.xml", "modules.xml", "workspace.xml").
+			AddChildFileGroup(NewFileGroup("inspectionProfiles", "Project_Default.xml").SetCustomDirName("a")).
+			AddChildFileGroup(NewFileGroup(".idea/example", "utils.go", "utils_test.go").SetCustomDirName("b")),
+	})
+	AssertError(err, "zip failed")
+}
+
+func TestZip2(t *testing.T) {
+	file, err := os.OpenFile("output.zip", os.O_CREATE|os.O_TRUNC|os.O_RDWR, 0644)
+	AssertError(err, "create output")
+
+	writer := zip.NewWriter(file)
+	defer func() {
+		CloseIgnoreErr(writer)
+		CloseIgnoreErr(file)
+	}()
+	_, err = writer.Create("a/")
+	AssertError(err, "create a/")
+	_, err = writer.Create("a/b/")
+	AssertError(err, "create a/b/")
+	_, err = writer.Create("a/")
+	AssertError(err, "create a/ again")
 }
