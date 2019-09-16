031fa0c08eea28e5b2b291127d0ee10998176491
Suzhen <suzhen@homeinpocket.net>
2019-09-16 14:01:58 +0800

    :recycle: refactor worker_pool, execute support ctx param

--- worker_pool.go
+++ worker_pool.go
@@ -2,6 +2,7 @@ package std
 
 import (
 	"container/list"
+	"context"
 	"fmt"
 	"runtime"
 	"sync"
@@ -13,7 +14,6 @@ type WorkerFunc = func()
 type WorkerPool struct {
 	name      string
 	cond      *sync.Cond
-	exitSig   chan bool
 	workN     int
 	workQ     *list.List
 	startFlag int32
@@ -26,7 +26,6 @@ func NewWorkerPool(name string, num int) *WorkerPool {
 	return &WorkerPool{
 		name:      name,
 		cond:      sync.NewCond(new(sync.Mutex)),
-		exitSig:   make(chan bool),
 		workN:     MinInt(num, runtime.NumCPU()),
 		workQ:     list.New(),
 		startFlag: 0,
@@ -41,11 +40,10 @@ func (this *WorkerPool) Submit(f WorkerFunc) {
 	this.cond.Broadcast()
 }
 
-func (this *WorkerPool) workerTask(wg *sync.WaitGroup) {
-	defer wg.Done()
+func (this *WorkerPool) workerTask(ctx context.Context) {
 	for {
 		select {
-		case <-this.exitSig:
+		case <-ctx.Done():
 			return
 		default:
 		}
@@ -69,30 +67,27 @@ func (this *WorkerPool) GetName() string {
 	return this.name
 }
 
-func (this *WorkerPool) Execute() {
+func (this *WorkerPool) Execute(ctx context.Context) {
 	Assert(atomic.CompareAndSwapInt32(&this.startFlag, 0, 1),
 		fmt.Sprintf("worker pool [%s] start multiple times...", this.name))
-	wg := new(sync.WaitGroup)
-	wg.Add(this.workN)
+	group := NewAsyncTaskGroup()
+	defer group.Wait()
 	for i := 0; i < this.workN; i++ {
-		go this.workerTask(wg)
+		group.AddTask(func() {
+			this.workerTask(ctx)
+		})
 	}
-	wg.Wait()
-}
-
-func (this *WorkerPool) Close() error {
-	this.cond.L.Lock()
-	defer this.cond.L.Unlock()
-	closed := false
-	select {
-	case <-this.exitSig:
-		closed = true
-	default:
-		closed = false
-	}
-	if !closed {
-		close(this.exitSig)
+	group.AddTask(func() {
+		// watcher to unlock,if ctx done
+		<-ctx.Done()
+		//
+		this.cond.L.Lock()
+		defer this.cond.L.Unlock()
+		// drop all task
+		for this.workQ.Len() != 0 {
+			this.workQ.Remove(this.workQ.Front())
+		}
+		// send exit signal
 		this.cond.Broadcast()
-	}
-	return nil
+	})
 }
