d665a4a9ea8e5320d1d74e26a7c8364070fd344d
Suzhen <suzhen@homeinpocket.net>
2019-12-18 12:41:58 +0800

    :white_check_mark: add context

--- worker_pool_test.go
+++ worker_pool_test.go
@@ -1,16 +1,19 @@
 package std
 
 import (
+	"context"
 	"runtime"
 	"testing"
 	"time"
 )
 
 func TestWorkerPool(t *testing.T) {
+	cancel, cancelFunc := context.WithCancel(context.Background())
+	defer cancelFunc()
 	pool := NewWorkerPool("tester", runtime.NumCPU())
 	pool.Submit(func() {
 		time.Sleep(time.Second * 5)
-		_ = pool.Close()
+		cancelFunc()
 	})
-	pool.Execute()
+	pool.Execute(cancel)
 }
