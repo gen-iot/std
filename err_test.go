ae94c1ad2eafe82a505f40b52ec7b3d6b7a5ce37
Suzhen <suzhen@homeinpocket.net>
2020-07-09 17:25:21 +0800

    :white_check_mark: add endian cast

--- err_test.go
+++ err_test.go
@@ -2,7 +2,9 @@ package std
 
 import (
 	"errors"
+	"reflect"
 	"testing"
+	"unsafe"
 )
 
 func TestErrorf(t *testing.T) {
@@ -17,6 +19,20 @@ func TestErrorWrap(t *testing.T) {
 	t.Log(ErrorWrap(errors.New("raw error"), "what fuck?"))
 }
 
-func TestError(t *testing.T) {
-	t.Log(Error("what fuck?"))
+func TestEndianCast(t *testing.T) {
+	//t.Log(Error("what fuck?"))
+	v := uint16(0xFF00)
+	slice := reflect.New(reflect.TypeOf(([]uint8)(nil)).Elem())
+	header := (*reflect.SliceHeader)(unsafe.Pointer(slice.Pointer()))
+	header.Cap = 2
+	header.Len = 2
+	header.Data = uintptr(unsafe.Pointer(&v))
+	xxx := *(*[]uint8)(unsafe.Pointer(header))
+	for _, v := range xxx {
+		t.Logf("%02X ", v)
+	}
+	t.Log("------")
+	v1 := uint8(v >> 8)
+	v2 := uint8(v)
+	t.Logf("%02X %02X", v1, v2)
 }
