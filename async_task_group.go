1a8e85cb8ce5ad9eb579583c582d00119ef8f68a
suzhen <suzhen@homeinpocket.net>
2020-09-09 12:51:41 +0800

    :sparkles: task group新增计数器

--- async_task_group.go
+++ async_task_group.go
@@ -1,10 +1,14 @@
 package std
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // best practice for wait_group
 type AsyncTaskGroup struct {
-	wg *sync.WaitGroup
+	wg      *sync.WaitGroup
+	taskCnt int32
 }
 
 func NewAsyncTaskGroup() *AsyncTaskGroup {
@@ -13,16 +17,27 @@ func NewAsyncTaskGroup() *AsyncTaskGroup {
 
 func NewAsyncTaskGroup2(wg *sync.WaitGroup) *AsyncTaskGroup {
 	Assert(wg != nil, "waitGroup is nil")
-	return &AsyncTaskGroup{wg: wg}
+	return &AsyncTaskGroup{
+		wg:      wg,
+		taskCnt: 0,
+	}
 }
 
 func (this *AsyncTaskGroup) AddTask(exe func()) {
 	Assert(exe != nil, "exe cant be nil")
+	atomic.AddInt32(&this.taskCnt, 1)
 	this.wg.Add(1)
 	go func() {
-		defer this.wg.Done()
+		defer func() {
+			this.wg.Done()
+			atomic.AddInt32(&this.taskCnt, -1)
+		}()
 		exe()
 	}()
+}
+
+func (this *AsyncTaskGroup) GetTotalTask() int {
+	return int(atomic.LoadInt32(&this.taskCnt))
 }
 
 func (this *AsyncTaskGroup) Wait() {
